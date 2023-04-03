0711f79b35dd3c9cab8af8a5f8c32b49a4664b4b
Oleksandr Korobko <alex.korobko.ua@gmail.com>
2023-04-03 14:40:32 +0300

    add useSpeechSynthesis hook

--- src/app/hooks/useSpeechSynthesis.ts
+++ src/app/hooks/useSpeechSynthesis.ts
@@ -1,21 +1,19 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+
+const speechVoices: SpeechSynthesisVoice[] = [];
+let globalSpeaking = false;
+
+if ('speechSynthesis' in window) {
+  const handleVoicesChanged = () => {
+    speechVoices.push(...speechSynthesis.getVoices());
+    console.log('handleVoicesChanged: ', speechVoices.length);
+  };
+
+  speechSynthesis.addEventListener('voiceschanged', handleVoicesChanged);
+}
 
 export const useSpeechSynthesis = () => {
-  const [isSpeaking, setIsSpeaking] = useState(false);
-  const [voices, setVoices] = useState<SpeechSynthesisVoice[]>([]);
-
-  useEffect(() => {
-    if ('speechSynthesis' in window) {
-      const handleVoicesChanged = () => {
-        setVoices(speechSynthesis.getVoices());
-      };
-
-      speechSynthesis.addEventListener('voiceschanged', handleVoicesChanged);
-      return () => {
-        speechSynthesis.removeEventListener('voiceschanged', handleVoicesChanged);
-      };
-    }
-  }, []);
+  const [isSpeaking, setIsSpeaking] = useState(globalSpeaking);
 
   const speak = (text: string, lang = 'en-US', voiceName = "Eddy (English (US))") => {
     if (!('speechSynthesis' in window)) {
@@ -23,34 +21,41 @@ export const useSpeechSynthesis = () => {
       return;
     }
 
+    if (globalSpeaking || isSpeaking) {
+      return;
+    }
+
     const utterance = new SpeechSynthesisUtterance(text);
     utterance.lang = lang;
     utterance.rate = 0.9;
 
-    const langVoices = voices
+    const langVoices = speechVoices
       .filter((voice) => voice.lang === lang);
 
-    console.log(langVoices)
+    utterance.voice = langVoices.find((voice) => voice.name === voiceName)
+      || langVoices[0];
 
-    utterance.voice = langVoices.find((voice) => voice.name === voiceName) || langVoices[langVoices.length - 1];
-
+    globalSpeaking = true;
     setIsSpeaking(true);
     speechSynthesis.speak(utterance);
 
     utterance.onend = () => {
       setIsSpeaking(false);
+      globalSpeaking = false;
     };
 
     utterance.onerror = (event) => {
       console.error('Speech synthesis error:', event);
       setIsSpeaking(false);
+      globalSpeaking = false;
     };
   };
 
   const stopSpeaking = () => {
-    if (isSpeaking) {
+    if (isSpeaking || globalSpeaking) {
       speechSynthesis.cancel();
       setIsSpeaking(false);
+      globalSpeaking = false;
     }
   };
 
